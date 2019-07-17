0b67bc15796dd6ca54a43e6cf43c498997a91ffe
Brent Rowland <brent.rowland@ttiltd.com>
2019-07-16 23:02:56 -0700

    Text out

--- spec/pdflib_spec.cr
+++ spec/pdflib_spec.cr
@@ -52,9 +52,32 @@ describe "PDFlib" do
       res.should eq 1
       PDFlib.begin_page_ext(doc, width, height, optlist)
 
-      PDFlib.moveto(doc, 72, 72)
-      PDFlib.lineto(doc, 144, 72)
+      PDFlib.moveto(doc, 72, height - 72)
+      PDFlib.lineto(doc, 144, height - 72)
       PDFlib.closepath_stroke(doc)
+
+      PDFlib.end_page_ext(doc, optlist)
+      PDFlib.end_document(doc, optlist)
+      PDFlib.delete(doc)
+      File.exists?(filename).should be_true
+      # File.delete(filename) if File.exists?(filename)
+    end
+  end
+
+  context "text out" do
+    filename = "test_text_out.pdf"
+    it "should move to position and draw text" do
+      File.delete(filename) if File.exists?(filename)
+      doc = PDFlib.new
+      PDFlib.set_option(doc, "stringformat=utf8")
+      res = PDFlib.begin_document(doc, filename, 0, optlist)
+      res.should eq 1
+      PDFlib.begin_page_ext(doc, width, height, optlist)
+
+      font_handle = PDFlib.load_font(doc, "Times-Roman", 0, "unicode", "")
+      PDFlib.set_text_option(doc, "font=#{font_handle} fontsize=12")
+      PDFlib.set_text_pos(doc, 72, height - 72)
+      PDFlib.show(doc, "Hello, World!")
 
       PDFlib.end_page_ext(doc, optlist)
       PDFlib.end_document(doc, optlist)
--- src/pdflib.cr
+++ src/pdflib.cr
@@ -13,10 +13,15 @@ lib PDFlib
   fun set_option = PDF_set_option(p : PDF*, optlist : LibC::Char*) : Void
   fun set_parameter = PDF_set_parameter(p : PDF*, optlist : LibC::Char*) : Void
   fun get_errmsg = PDF_get_errmsg(p : PDF*) : LibC::Char*
-  fun moveto = PDF_moveto(p : PDF*, x : LibC::Double, y : LibC::Double)
-  fun lineto = PDF_lineto(p : PDF*, x : LibC::Double, y : LibC::Double)
-  fun closepath = PDF_closepath(p : PDF*)
-  fun closepath_fill_stroke = PDF_closepath_fill_stroke(p : PDF*)
-  fun closepath_stroke = PDF_closepath_stroke(p : PDF*)
-  fun stroke = PDF_stroke(p : PDF*)
+  fun moveto = PDF_moveto(p : PDF*, x : LibC::Double, y : LibC::Double) : Void
+  fun lineto = PDF_lineto(p : PDF*, x : LibC::Double, y : LibC::Double) : Void
+  fun closepath = PDF_closepath(p : PDF*) : Void
+  fun closepath_fill_stroke = PDF_closepath_fill_stroke(p : PDF*) : Void
+  fun closepath_stroke = PDF_closepath_stroke(p : PDF*) : Void
+  fun stroke = PDF_stroke(p : PDF*) : Void
+  fun load_font = PDF_load_font(p : PDF*, fontname : LibC::Char*, len : LibC::Int, encoding : LibC::Char*, optlist : LibC::Char*) : LibC::Int
+  fun set_text_option = PDF_set_text_option(p : PDF*, optlist : LibC::Char*) : Void
+  fun set_text_pos = PDF_set_text_pos(p : PDF*, x : LibC::Double, y : LibC::Double) : Void
+  fun show = PDF_show(p : PDF*, text : LibC::Char*) : Void
+  fun show2 = PDF_show2(p : PDF*, text : LibC::Char*, len : LibC::Int)
 end
